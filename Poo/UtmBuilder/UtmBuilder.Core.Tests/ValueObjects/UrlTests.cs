a1c1734d7528592a9fa3207b6770d2233ab0f20b
farleybsd <farley.t.i@hotmail.com>
2023-05-05 15:09:59 -0300

    TESTS

---
+++ Poo/UtmBuilder/UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtmBuilder.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class UrlTests
+    {
+        [TestMethod("Deve Retornar uma execacao quando uma Url for invalida")]
+        [TestCategory("Testè Da Url")]
+        public void Dado_uma_url_invalida_deve_retornar_uma_exceca0()
+        {
+
+        }
+    }
+}
